7cf00c3502e961d0573961d91299750f410784a5
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2022-02-02 11:09:33 -0300

    [BACK] Criando server e routes

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./start/routes');
+//const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.use(express.json());
+
+//para acessar as  rotas 
+app.use(routes);
+
+/* app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false})); */
+
+/* app.get('/', (req, res)=>{
+    res.send('OK');
+}); */
+
+app.listen(3333);
